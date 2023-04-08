ac11c3d9decc48b4769787eb83ba078cd864206c
Lu Miao <miaolphoebe@gmail.com>
2023-04-07 23:42:38 -0400

    fix tests

--- server/api/gatekeepingMiddleware.js
+++ server/api/gatekeepingMiddleware.js
@@ -1,18 +1,18 @@
 const {
   models: { User },
-} = require("../db");
+} = require('../db');
 
 const requireToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
     const user = await User.findByToken(token);
     req.user = user;
-    next()
+    next();
   } catch (error) {
     next(error);
   }
 };
 
 module.exports = {
-  requireToken
-}
+  requireToken,
+};
--- server/db/models/User.spec.js
+++ server/db/models/User.spec.js
@@ -1,12 +1,16 @@
 /* global describe beforeEach it */
 
 const { expect } = require("chai");
+const chai = require("chai");
 const {
   db,
   models: { User },
 } = require("../index");
 const jwt = require("jsonwebtoken");
+const chaiHttp = require("chai-http");
 const seed = require("../../../script/seed");
+let app = require("../../app");
+chai.use(chaiHttp);
 
 describe("User model", () => {
   let users;
@@ -18,10 +22,19 @@ describe("User model", () => {
     describe("generateToken", () => {
       it("returns a token with the id of the user", async () => {
         const token = await users.cody.generateToken();
-        const { id } = await jwt.verify(token, process.env.JWT);
-        expect(id).to.equal(users.cody.id);
+
+        chai
+          .request(app)
+          .get("/users")
+          .set({ Authorization: `Bearer ${token}` })
+          .end(async (err, res) => {
+            expect(res.statusCode).to.equal(200);
+
+            const { id } = await jwt.verify(token, process.env.SECRET);
+            expect(id).to.equal(users.cody.id);
+          });
       });
-    }); // end describe('correctPassword')
+    });
     describe("authenticate", () => {
       let user;
       beforeEach(
@@ -29,6 +42,7 @@ describe("User model", () => {
           (user = await User.create({
             username: "lucy",
             password: "loo",
+            email: "lucy@email.com",
           }))
       );
       describe("with correct credentials", () => {
@@ -36,6 +50,7 @@ describe("User model", () => {
           const token = await User.authenticate({
             username: "lucy",
             password: "loo",
+            email: "lucy@email.com",
           });
           expect(token).to.be.ok;
         });
@@ -46,6 +61,7 @@ describe("User model", () => {
             await User.authenticate({
               username: "lucy@gmail.com",
               password: "123",
+              email: "lucy@email.com",
             });
             throw "nooo";
           } catch (ex) {
